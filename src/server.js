e2f776fab680ee98ea63464304526dec3d01d4ca
Ivan-Leonardi <ivanthaisjesus@gmail.com>
2024-09-13 06:09:16 -0300

    feat: add allowedHeaders CORS

--- src/server.js
+++ src/server.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(cors({
   origin: "https://todo-experts.vercel.app",
   methods: "GET, PUT, PATCH, POST, DELETE",
-  allowedHeaders: "Content-Type, Authorization",
+  allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
 }));
 
