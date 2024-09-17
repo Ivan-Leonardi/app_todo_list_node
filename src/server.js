7cebc7781d784d63810ca57b21afc745206a7202
Ivan-Leonardi <ivanthaisjesus@gmail.com>
2024-09-16 22:14:11 -0300

    updated url

--- src/server.js
+++ src/server.js
@@ -9,7 +9,7 @@ const app = express();
 
 app.use(
     cors({
-        origin: "https://todo-experts.netlify.app",
+        origin: "https://todo-experts.vercel.app",
         methods: "GET, PUT, PATCH, POST, DELETE",
         allowedHeaders: ["Content-Type, Authorization"],
         credentials: true,
